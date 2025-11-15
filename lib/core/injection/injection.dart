8c9c0b74bbe90328b50117476bc01cb3b3ec8732
XeldarAlz <xeldaralz@hotmail.com>
2025-11-15 11:24:35 +0300

    refactor: update dependency injection to use local data source
    - Remove RecipeRemoteDataSource and Translator dependencies
    - Remove NetworkInfo and http.Client dependencies
    - Register RecipeLocalDataSource instead
    - Simplify dependency injection setup

--- lib/core/injection/injection.dart
+++ lib/core/injection/injection.dart
@@ -1,7 +1,5 @@
 import 'package:get_it/get_it.dart';
-import 'package:http/http.dart' as http;
-import 'package:connectivity_plus/connectivity_plus.dart';
-import '../../features/recipe/data/datasources/recipe_remote_data_source.dart';
+import '../../features/recipe/data/datasources/recipe_local_data_source.dart';
 import '../../features/recipe/data/repositories/recipe_repository_impl.dart';
 import '../../features/recipe/domain/repositories/recipe_repository.dart';
 import '../../features/recipe/domain/usecases/get_random_recipe.dart';
@@ -9,40 +7,20 @@ import '../../features/recipe/domain/usecases/get_recipe_by_name.dart';
 import '../../features/recipe/domain/usecases/get_recipes_by_ingredients.dart';
 import '../../features/recipe/presentation/bloc/recipe_bloc.dart';
 import '../../features/ingredients/presentation/bloc/ingredients_bloc.dart';
-import '../network/network_info.dart';
-import '../network/translator.dart';
 
 final getIt = GetIt.instance;
 
 Future<void> initDependencies() async {
-  // Core
-  getIt.registerLazySingleton<NetworkInfo>(
-    () => NetworkInfoImpl(Connectivity()),
-  );
-
-  // External
-  getIt.registerLazySingleton(() => http.Client());
-  getIt.registerLazySingleton(() => Connectivity());
-
-  // Core - Translator
-  getIt.registerLazySingleton<Translator>(
-    () => Translator(client: getIt()),
-  );
-
   // Features - Recipe
   // Data sources
-  getIt.registerLazySingleton<RecipeRemoteDataSource>(
-    () => RecipeRemoteDataSourceImpl(
-      client: getIt(),
-      translator: getIt(),
-    ),
+  getIt.registerLazySingleton<RecipeLocalDataSource>(
+    () => RecipeLocalDataSourceImpl(),
   );
 
   // Repository
   getIt.registerLazySingleton<RecipeRepository>(
     () => RecipeRepositoryImpl(
-      remoteDataSource: getIt(),
-      networkInfo: getIt(),
+      localDataSource: getIt(),
     ),
   );
 
